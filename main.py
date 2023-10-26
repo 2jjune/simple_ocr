441de59440237e7f3320d6f5dee4443c411e9e9d
2jjune <145344483+2jjune@users.noreply.github.com>
2023-10-26 17:30:51 +0900

    add files

---
+++ main.py
@@ -0,0 +1,11 @@
+import pytesseract
+import cv2
+import matplotlib.pyplot as plt
+
+
+path = './tmp.jpg'
+image = cv2.imread(path)
+rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+text = pytesseract.image_to_string(rgb_image, lang='kor+eng')
+print(text)
